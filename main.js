3b2745221d36128206aa1c9753499c897b2e7795
Louis <cassany.louis@gmail.com>
2023-04-06 11:46:01 +0200

    test

--- main.js
+++ main.js
@@ -15,6 +15,55 @@ let container;
 let camera, scene, renderer;
 let controls, water, sun;
 
+const elements = {
+  vue_aerienne: {
+    position: { x: -255, y: 232, z: -257 },
+    target: { x: 0, y: 10, z: 0 },
+    title: "Vue aérienne",
+    description: "Une belle vue aérienne du bateau."
+  },
+  poste_de_pilotage: {
+    position: { x: -28, y: 20, z: 4 },
+    target: { x: -2, y: 8, z: -3 },
+    title: "Poste de pilotage",
+    description: "Un siège et un ordinateur."
+  },
+  sea_kite: {
+    position: { x: -25, y: 17, z: -3 },
+    target: { x: -8, y: 28, z: 8 },
+    title: "Sea kite",
+    description: "Une belle voile."
+  },
+  mat_de_lancement: {
+    position: { x: -17, y: 40, z: -33 },
+    target: { x: -8, y: 28, z: 8 },
+    title: "Mat de lancement",
+    description: "Un mat en carbone."
+  },
+  platine_de_traction: {
+    position: { x: -1, y: 17, z: -7 },
+    target: { x: -7, y: 11, z: 0 },
+    title: "Platine de traction",
+    description: "La platine tire le bateau."
+  },
+  panneaux_solaires: {
+    position: { x: 37, y: 54, z: 1 },
+    target: { x: 27, y: 0, z: 1 },
+    title: "Panneaux solaires",
+    description: "Des panneaux pour charger les batteries."
+  },
+  kitebox: {
+    position: { x: 0, y: 17, z: 6 },
+    target: { x: 9, y: 9, z: 4 },
+    title: "Kitebox",
+    description: "Une boite en métal."
+  }
+}
+
+const titleDiv = document.getElementById("title")
+const descriptionDiv = document.getElementById("description")
+
+
 window.addEventListener('load', function () {
   init();
   animate();
@@ -34,7 +83,7 @@ function init() {
 
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
+  camera = new THREE.PerspectiveCamera(55, document.getElementById("container").clientWidth / document.getElementById("container").clientHeight, 1, 20000);
   camera.position.set(30, 30, 100);
 
 
@@ -149,36 +198,18 @@ function init() {
     window.alert(`addView({x:${Math.floor(controls.object.position.x)}, y:${Math.floor(controls.object.position.y)}, z:${Math.floor(controls.object.position.z)}}, {x:${Math.floor(controls.target.x)}, y:${Math.floor(controls.target.y)}, z:${Math.floor(controls.target.z)}}, "Nom à afficher");`)
   })
 
-  // addView("arriere", { x: -75, y: 5, z: 35 }, { x: -35, y: 12, z: 30 }, "Arrière");
-  // addView("babord", { x: 61, y: 28, z: -77 }, { x: 0, y: 10, z: 0 }, "Babord");
-  // addView("avant", { x: 111, y: 13, z: 2 }, { x: 0, y: 10, z: 0 }, "Avant");
-  // addView("retour", { x: 11, y: 110, z: 233 }, { x: 0, y: 10, z: 0 }, "Retour");
+  // addView({ x: -255, y: 232, z: -257 }, { x: 0, y: 10, z: 0 }, "Vue aérienne", "vue_aerienne");
+  // addView({ x: -28, y: 20, z: 4 }, { x: -2, y: 8, z: -3 }, "Poste de pilotage", "poste_de_pilotage");
+  // addView({ x: -25, y: 17, z: -3 }, { x: -8, y: 28, z: 8 }, "Sea kite", "sea_kite");
+  // addView({ x: -17, y: 40, z: -33 }, { x: -8, y: 28, z: 8 }, "Mat de lancement", "mat_de_lancement");
+  // addView({ x: -1, y: 17, z: -7 }, { x: -7, y: 11, z: 0 }, "Platine de traction", "platine_de_traction");
+  // addView({ x: 37, y: 54, z: 1 }, { x: 27, y: 0, z: 1 }, "Panneaux solaires", "panneaux_solaires");
+  // addView({ x: 0, y: 17, z: 6 }, { x: 9, y: 9, z: 4 }, "Kitebox", "kitebox");
 
-  addView({ x: -28, y: 20, z: 4 }, { x: -2, y: 8, z: -3 }, "Poste de pilotage");
-  addView({ x: 0, y: 17, z: 6 }, { x: 9, y: 9, z: 4 }, "Kitebox");
-  addView({ x: -1, y: 17, z: -7 }, { x: -7, y: 11, z: 0 }, "Platine de traction");
-  addView({ x: 37, y: 54, z: 1 }, { x: 27, y: 0, z: 1 }, "Panneaux solaires");
-  addView({ x: -17, y: 40, z: -33 }, { x: -8, y: 28, z: 8 }, "Mat de lancement");
-  addView({ x: -25, y: 17, z: -3 }, { x: -8, y: 28, z: 8 }, "Sea kite");
-  addView({ x: -255, y: 232, z: -257 }, { x: 0, y: 10, z: 0 }, "Vue aérienne");
-
-
-  function addView(position, target, nom) {
-    const list = document.getElementById("views");
-    const li = document.createElement("div");
-    li.classList.add(["hover:text-red-600"])
-    li.innerHTML = nom;
-    list.appendChild(li)
-    li.addEventListener("click", () => {
-      new TWEEN.Tween(controls.object.position).to(position,
-        2000).easing(TWEEN.Easing.Cubic.Out)
-        .start()
-      new TWEEN.Tween(controls.target).to(target,
-        2000).easing(TWEEN.Easing.Cubic.Out)
-        .start()
-    });
+  for (const key of Object.keys(elements)) {
+    const element = elements[key];
+    addView({ ...element, id: key })
   }
-
 }
 
 function onWindowResize() {
@@ -199,9 +230,31 @@ function animate() {
 }
 
 function render() {
-
   water.material.uniforms['time'].value += 1.0 / 60.0;
-
   renderer.render(scene, camera);
+}
 
+function addView({ position, target, title, id, description }) {
+  const list = document.getElementById("views");
+  const li = document.createElement("div");
+  li.classList.add(["hover:text-slate-400"])
+  li.classList.add(["cursor-pointer"])
+  li.innerHTML = title;
+  li.id = id
+  list.appendChild(li)
+  li.addEventListener("click", (event) => {
+    new TWEEN.Tween(controls.object.position).to(position,
+      2000).easing(TWEEN.Easing.Cubic.Out)
+      .start()
+    new TWEEN.Tween(controls.target).to(target,
+      2000).easing(TWEEN.Easing.Cubic.Out)
+      .start()
+    for (const element of event.target.parentNode.children) {
+      element.classList.remove("text-slate-400")
+      console.log(element)
+    }
+    event.target.classList.add("text-slate-400")
+    descriptionDiv.innerHTML = description;
+    titleDiv.innerHTML = title
+  });
 }
